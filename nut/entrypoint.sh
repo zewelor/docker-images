659877be444db72a19634a10cccd7a3181034715
zewelor <zewelor@gmail.com>
2026-05-21 20:56:27 +0100

    Simplify NUT entrypoint

--- nut/entrypoint.sh
+++ nut/entrypoint.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 set -e
 
-# If no arguments are provided, or if the first argument starts with a hyphen,
-# run the default startup (driver + daemon).
-if [ $# -eq 0 ] || [ "${1#-}" != "$1" ]; then
-    echo "Starting NUT driver..."
-    /usr/sbin/upsdrvctl -u root start
+echo "Starting NUT driver..."
+/usr/sbin/upsdrvctl -u root start
 
-    echo "Starting NUT daemon..."
-    exec /usr/sbin/upsd -u root -D "$@"
-fi
-
-# Otherwise, execute the user's custom command (e.g. upsc, sh, etc.)
-exec "$@"
+echo "Starting NUT daemon..."
+exec /usr/sbin/upsd -u root -D "$@"
